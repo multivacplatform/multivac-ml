fcd54d2e311f958e97f11ebc0c67471b506d7e24
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-10-10 14:08:11 +0200

    Build Spark session

---
+++ demo/src/main/scala/SessionBuilder.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.sql.SparkSession
+
+object SessionBuilder {
+  def buildSession(): SparkSession = {
+
+
+    val spark: SparkSession = SparkSession.builder
+      .appName("multivac-ml-demo")
+      .master("local[*]")
+      .enableHiveSupport()
+      .config("spark.driver.memory", "4G")
+      .config("spark.kryoserializer.buffer.max","200M")
+      .config("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate
+
+    spark.sparkContext.setLogLevel("INFO")
+
+    spark
+  }
+}
