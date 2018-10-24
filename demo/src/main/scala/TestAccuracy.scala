52d58645f6b725acf54ad14e22daf2d5a0973950
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-10-24 16:29:28 +0200

    Add POS accurracy test

---
+++ demo/src/main/scala/TestAccuracy.scala
@@ -0,0 +1,127 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions._
+
+import scala.collection.mutable.ArrayBuffer
+
+object TestAccuracy {
+  def posTaggerEnglish_ml(
+                           spark: SparkSession,
+                           testPath: String,
+                           modelPath: String
+                         ): Unit = {
+    import spark.implicits._
+
+
+    val testInput = spark.read.text(testPath).as[String]
+    val extractedTokensTags = testInput.map(s => s.split("\t")
+      .filter(x => !x.startsWith("#"))).filter(x => x.length > 0)
+      .map{x => if(x.length > 1){x(1) + "_" + x(3)} else{"endOfLine"}}
+      .map(x => x.mkString)
+      .reduce((s1, s2) => s1 + " " + s2).split(" endOfLine | endOfLine")
+
+    val testTokensTagsDF = spark.sparkContext.parallelize(extractedTokensTags)
+      .toDF("arrays")
+      .withColumn("id", monotonically_increasing_id)
+      .withColumn("testTokens", extractTokens($"arrays"))
+      .withColumn("testTags", extractTags($"arrays"))
+      .drop("arrays")
+
+    println("Count of extracted POS tags DF: ", testTokensTagsDF.count())
+    testTokensTagsDF.filter("id=4").show(false)
+
+    //Convert CoNLL-U to Text for training the test dataset
+    val testSentencesDF = testInput
+      .map(s => s.split("\t").filter(x => x.startsWith("# text")))
+      .flatMap(x => x)
+      .map(x => x.replace("# text = ", ""))
+      .filter(x => x.length > 0)
+      .toDF("content")
+      .withColumn("id", monotonically_increasing_id)
+
+    println("Count of extracted sentences DF: ", testSentencesDF.count())
+    testSentencesDF.filter("id=4").show(false)
+    //Load pre-trained pos model
+    val pipeLinePOSTaggerModel = PipelineModel.read.load(modelPath)
+
+    val manualPipelineDF = pipeLinePOSTaggerModel.transform(testSentencesDF)
+      .select(
+        $"id",
+        $"token.result".alias("predictedTokens"),
+        $"pos.result".alias("predictedTags")
+      )
+    println("Count of trained sentences DF: ", manualPipelineDF.count())
+    manualPipelineDF.filter("id=4").show(false)
+
+
+    val joinedDF = manualPipelineDF
+      .join(testTokensTagsDF, Seq("id"))
+      .withColumn("predictedTokensLength", calLengthOfArray($"predictedTokens"))
+      .withColumn("predictedTagsLength", calLengthOfArray($"predictedTags"))
+      .withColumn("testTokensLength", calLengthOfArray($"testTokens"))
+      .withColumn("testTagsLength", calLengthOfArray($"testTags"))
+      .withColumn("equalTags", col("predictedTagsLength") === col("testTagsLength"))
+
+    joinedDF.show
+    joinedDF.filter("id=4").show(false)
+    joinedDF.printSchema()
+
+    // Due to different methods of tokenizing, the number of tokens may be different, we need the ones that are equal for accuracy test.
+    println("Equal number of tags between test and training:", joinedDF.filter("equalTags = true").count)
+
+    val accuracyDF = joinedDF.filter("equalTags = true")
+      .withColumn("correctPredictTags", compareTwoTagsArray($"testTags", $"predictedTags"))
+
+    val sumOfAllTags = accuracyDF.agg(
+      sum("testTagsLength").as("sum_testTagsLength"),
+      sum("predictedTagsLength").as("sum_predictedTagsLength"),
+      sum("correctPredictTags").as("sum_correctPredictTags")
+    ).withColumn("accuracy", ($"sum_correctPredictTags" * 100) / $"sum_predictedTagsLength")
+    sumOfAllTags.first()
+    sumOfAllTags.show()
+
+  }
+
+  private def extractTokens= udf { docs: String =>
+    var tokensArray = ArrayBuffer[String]()
+    val splitedArray = docs.split("\\s+")
+    for(e <- splitedArray){
+      tokensArray += e.split("_")(0)
+    }
+    tokensArray
+  }
+  private def extractTags= udf { docs: String =>
+    var tagsArray = ArrayBuffer[String]()
+    val splitedArray = docs.split("\\s+")
+    for(e <- splitedArray){
+      tagsArray += e.split("_")(1)
+    }
+    tagsArray
+  }
+  private def calLengthOfArray= udf { docs: Seq[String] =>
+    docs.length
+  }
+  private def compareTwoTagsArray= udf { (testTags: Seq[String], predictTags: Seq[String]) =>
+    var correctTagsCount = 0
+    val testTagsWithIndex = testTags.zipWithIndex.map{case (k,v) => (v,k)}
+    val predictTagsWithIndex = predictTags.zipWithIndex.map{case (k,v) => (v,k)}
+
+    for (e <- testTagsWithIndex){
+      if (predictTagsWithIndex.find(_._1 == e._1).get._2 == e._2){
+        correctTagsCount+=1
+      }
+      correctTagsCount
+    }
+    /*for (i <- testTags.indices){
+      if(i >= predictTags.length && i >= testTags.length) {
+        if(testTags(i) == predictTags(i))
+          correctTagsCount+=1
+      } else{
+        correctTagsCount = 0
+      }
+    }*/
+    correctTagsCount
+  }
+}
