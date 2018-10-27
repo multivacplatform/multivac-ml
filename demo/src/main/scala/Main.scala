6f339c4e62f94099a82ce56fa12f01bf0e3dc7ae
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-10-27 17:27:29 +0200

    Update demo/src/main/scala/Main.scala

--- demo/src/main/scala/Main.scala
+++ demo/src/main/scala/Main.scala
@@ -5,5 +5,8 @@ object Main {
     MultivacDemo.word2Vec_ml(spark, "src/main/resources/models/multivac_word2vec_ml_200k")
     MultivacDemo.posTaggerEnglish_ml(spark, "src/main/resources/models/multivac_nlp_pos_UD_English-EWT")
     MultivacDemo.posTaggerFrench_ml(spark, "src/main/resources/models/multivac_nlp_pos_UD_French-GSD")
+    TestAccuracy.posTaggerEnglish_ml(spark, "src/main/resources/data/en_ewt-ud-test.conllu", "src/main/resources/models/multivac_nlp_pos_UD_English-EWT")
+    // Not working as it's expected
+    //TestAccuracy.posTaggerFrench_ml(spark, "src/main/resources/data/fr_gsd-ud-test.conllu", "src/main/resources/models/multivac_nlp_pos_UD_French-GSD")
   }
 }
