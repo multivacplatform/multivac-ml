13c41506d65c5a69ea8f8670928db67a881239ff
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-12-01 22:58:10 +0100

    Update MultivacDemo.scala

--- demo/src/main/scala/MultivacDemo.scala
+++ demo/src/main/scala/MultivacDemo.scala
@@ -31,7 +31,7 @@ object MultivacDemo {
   def word2Vec_ml(spark: SparkSession, inputPath: String): Unit = {
 
     val pipeLineWord2VecModel = PipelineModel.read.load(inputPath)
-    val word2VecModel = pipeLineWord2VecModel.stages.last.asInstanceOf[Word2VecModel]
+    val word2VecModel = pipeLineWord2VecModel.stages.lastOption.asInstanceOf[Word2VecModel]
 
     word2VecModel.findSynonyms("climate change", 10).show(false)
 
