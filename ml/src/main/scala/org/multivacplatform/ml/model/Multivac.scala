28a927310ea822778be0cbac7ca5b3f667775259
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-12-01 22:47:46 +0100

    Update Multivac.scala

--- ml/src/main/scala/org/multivacplatform/ml/model/Multivac.scala
+++ ml/src/main/scala/org/multivacplatform/ml/model/Multivac.scala
@@ -1,28 +1,94 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018. Maziyar Panahi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.multivacplatform.ml.model
 
-import org.apache.spark.ml.PipelineModel
-import org.apache.spark.sql.DataFrame
+import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
+import com.johnsnowlabs.nlp.annotator.{PerceptronApproach, SentenceDetector, Tokenizer}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.rdd.RDD
+import org.multivacplatform.ml.util._
 
 class Multivac {
-  def init(lang: String): PipelineModel = {
-    var mlPath: String = ""
 
-    lang match {
-      case "en" => mlPath = "src/main/resources/multivac-ml/nlp/multivac_nlp_pos_UD_English-EWT"
-      case "fr" => mlPath = "src/main/resources/multivac-ml/nlp/multivac_nlp_pos_UD_French-GSD"
-    }
+  /** Train
+    *
+    * @note
+    * @param inputDataset input RDD[String] from `spark.sparkContext.textFile`
+    * @return Array[String] to be saved for training `Spark-NLP`
+    */
+  def train(inputDataset: RDD[String], iterationNum: Int, textColName: String): PipelineModel = {
 
-    val pipeLinePOSTaggerModel = PipelineModel.read.load(mlPath)
-    pipeLinePOSTaggerModel
+    val spark = ResourceHelper.spark
 
-  }
+    val applicationId = spark.sparkContext.applicationId
 
-  def apply(pipeLinePOSTaggerModel: PipelineModel, inputDataFrame: DataFrame): DataFrame = {
-    val transformedDataFrame = pipeLinePOSTaggerModel.transform(inputDataFrame)
-    transformedDataFrame
-  }
+    val conllOutputPath = s"/tmp/$applicationId/english_universal_tags"
 
-  def testFunc(name: String): String = {
-    name + " hello!"
+    val conlluConverterClass = new ConllConverter
+
+    val taggedConnlText = conlluConverterClass.extractingTagsInConllu(inputDataset)
+    spark.sparkContext.parallelize(taggedConnlText).repartition(5).saveAsTextFile(conllOutputPath)
+
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol(textColName)
+      .setOutputCol("document")
+
+    val sentenceDetector = new SentenceDetector()
+      .setInputCols(Array("document"))
+      .setOutputCol("sentence")
+
+    val tokenizer = new Tokenizer()
+      .setInputCols(Array("sentence"))
+      .setOutputCol("token")
+
+    // POS tagging
+    val posOptions = Map("format" -> "text", "repartition" -> "1")
+    val posTagger = new PerceptronApproach()
+      .setNIterations(iterationNum)
+      .setInputCols(Array("sentence", "token"))
+      .setOutputCol("pos")
+      .setCorpus(path = conllOutputPath, delimiter = "_", options = posOptions)
+
+    // Finisher
+    val tokenFinisher = new Finisher()
+      .setInputCols("token")
+      .setOutputCols("token_array")
+      .setCleanAnnotations(false)
+      .setOutputAsArray(true)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(
+        documentAssembler,
+        sentenceDetector,
+        tokenizer,
+        posTagger,
+        tokenFinisher
+      ))
+
+    import spark.implicits._
+
+    pipeline.fit(Seq.empty[String].toDF(textColName))
   }
 }
