f1c786f87bd0008c64f23901a2db510fb3e6b970
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-12-01 19:18:28 +0100

    Create ContentProvider.scala

---
+++ ml/src/test/scala/org/multivacplatform/nlp/util/ContentProvider.scala
@@ -0,0 +1,46 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018. Maziyar Panahi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.multivacplatform.nlp.util
+
+object ContentProvider {
+
+  val sbdTestParagraph: String = "In the third category he included those Brothers (the majority) who saw nothing " +
+    "in Freemasonry but the external forms and ceremonies, and prized the strict performance of these forms without " +
+    "troubling about their purport or significance.@@ Such were Willarski and even the Grand Master of the principal " +
+    "lodge.@@ Finally, to the fourth category also a great many Brothers belonged, particularly those who had lately " +
+    "joined.@@ These according to Pierre's observations were men who had no belief in anything, nor desire for anything, " +
+    "but joined the Freemasons merely to associate with the wealthy young Brothers who were influential through their" +
+    " connections or rank, and of whom there were very many in the lodge.@@ Pierre began to feel dissatisfied with what" +
+    " he was doing.@@ Freemasonry, at any rate as he saw it here, sometimes seemed to him based merely on externals.@@" +
+    " He did not think of doubting Freemasonry itself, but suspected that Russian Masonry had taken a wrong path" +
+    " and deviated from its original principles.@@ And so toward the end of the year he went abroad to be initiated" +
+    " into the higher secrets of the order.@@ What is to be done in these circumstances?@@ To favor revolutions, overthrow" +
+    " everything, repel force by force?@@ No!@@ We are very far from that.@@ Every violent reform deserves censure, for it" +
+    " quite fails to remedy evil while men remain what they are, and also because wisdom needs no violence.@@ \"But" +
+    " what is there in running across it like that?\" said Ilagin's groom.@@ \"Once she had missed it and turned it" +
+    " away, any mongrel could take it,\" Ilagin was saying at the same time, breathless from his gallop and his" +
+    " excitement."
+
+}
