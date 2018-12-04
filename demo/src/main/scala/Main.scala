3e2843acddb60e07e08521af0214c9b5fe81da9d
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-12-04 12:15:20 +0100

    Add demo of how to train a model for POS tagging

--- demo/src/main/scala/Main.scala
+++ demo/src/main/scala/Main.scala
@@ -26,14 +26,23 @@ object Main {
   def main(args: Array[String]): Unit = {
     val spark = SessionBuilder.buildSession()
 
+
     MultivacDemo.word2Vec_ml(spark, "demo/src/main/resources/models/multivac_word2vec_ml_200k")
     MultivacDemo.posTaggerEnglish_ml(spark, "demo/src/main/resources/models/multivac_nlp_pos_UD_English-EWT")
     MultivacDemo.posTaggerFrench_ml(spark, "demo/src/main/resources/models/multivac_nlp_pos_UD_French-GSD")
+
+    TrainModel.posTaggerEnglish_ml(
+      "./data/ud-treebanks-v2.3/en_ewt-ud-train.conllu",
+      8,
+      "content"
+    )
+
     TestAccuracy.posTaggerEnglish_ml(
       spark,
-      "demo/src/main/resources/data/en_ewt-ud-test.conllu",
+      "./data/ud-treebanks-v2.3/en_ewt-ud-test.conllu",
       "demo/src/main/resources/models/multivac_nlp_pos_UD_English-EWT"
     )
+
     // Not working as it's expected
     //TestAccuracy.posTaggerFrench_ml(spark, "src/main/resources/data/fr_gsd-ud-test.conllu", "src/main/resources/models/multivac_nlp_pos_UD_French-GSD")
     spark.close()
