ce564808423c52e000d75f6bbc13a8265cc425e2
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-12-06 16:48:24 +0100

    Use object to declare the class to be reachable by the classloader

--- ml/src/main/scala/org/multivacplatform/ml/model/Multivac.scala
+++ ml/src/main/scala/org/multivacplatform/ml/model/Multivac.scala
@@ -24,79 +24,8 @@
 
 package org.multivacplatform.ml.model
 
-import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
-import com.johnsnowlabs.nlp.annotator.{PerceptronApproach, SentenceDetector, Tokenizer}
-import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.multivacplatform.ml.util._
+object Multivac {
 
-import ResourceHelper.spark.implicits._
+  val nlp = new MultivacNLPModel
 
-class Multivac {
-
-  private val spark = ResourceHelper.spark
-  private val applicationId = spark.sparkContext.applicationId
-  private val defaultConllOutputPath = s"./data/english_universal_tags/$applicationId"
-  /** Train
-    *
-    * @note
-    * @param inputCoNNLFilePath path to ConLL file to train POS Model
-    * @param outputConllFilePath output path to write converted CoNLL files `default: ./data/english_universal_tags/$applicationId`
-    * @param iterationNum number of iteration to train POS model
-    * @param textColName the name of column that contains the text to predict their POS tags
-    * @return Array[String] to be saved for training `Spark-NLP`
-    */
-  def train(inputCoNNLFilePath: String, outputConllFilePath: String = defaultConllOutputPath, iterationNum: Int = 5, textColName: String): PipelineModel = {
-
-    val conlluConverterClass = new CoNLLToPOSTextConverter
-
-    val taggedConnlTextDF = conlluConverterClass.extractingTagsInConllu(inputCoNNLFilePath, "pos_tagged")
-
-    taggedConnlTextDF.select("pos_tagged").coalesce(1).write.mode("OverWrite").text(outputConllFilePath)
-//    spark.sparkContext.parallelize(taggedConnlText).repartition(5).saveAsTextFile(outputConllFilePath)
-
-    val documentAssembler = new DocumentAssembler()
-      .setInputCol(textColName)
-      .setOutputCol("document")
-
-    val sentenceDetector = new SentenceDetector()
-      .setInputCols(Array("document"))
-      .setOutputCol("sentence")
-
-    val tokenizer = new Tokenizer()
-      .setInputCols(Array("sentence"))
-      .setOutputCol("token")
-
-    // POS tagging
-    val posOptions = Map("format" -> "text", "repartition" -> "1")
-    val posTagger = new PerceptronApproach()
-      .setNIterations(iterationNum)
-      .setInputCols(Array("sentence", "token"))
-      .setOutputCol("pos")
-      .setCorpus(path = s"$outputConllFilePath/*", delimiter = "_", options = posOptions)
-
-    // Finishers
-    val tokenFinisher = new Finisher()
-      .setInputCols("token")
-      .setOutputCols("token_array")
-      .setCleanAnnotations(false)
-      .setOutputAsArray(true)
-
-    val posFinisher = new Finisher()
-      .setInputCols("pos")
-      .setOutputCols("pos_array")
-      .setCleanAnnotations(false)
-      .setOutputAsArray(true)
-
-    val pipeline = new Pipeline()
-      .setStages(Array(
-        documentAssembler,
-        sentenceDetector,
-        tokenizer,
-        posTagger,
-        tokenFinisher,
-        posFinisher
-      ))
-
-    pipeline.fit(Seq.empty[String].toDF(textColName))
-  }
 }
