b1b2013676d37a310ac11b9295a89391eb5ce05b
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-12-27 14:01:37 +0100

    Update CoNLLToDataframe.scala

--- ml/src/main/scala/org/multivacplatform/ml/util/CoNLLToDataframe.scala
+++ ml/src/main/scala/org/multivacplatform/ml/util/CoNLLToDataframe.scala
@@ -1,3 +1,13 @@
+package org.multivacplatform.ml.util
+
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
+import org.apache.hadoop.mapreduce.Job
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
+import scala.collection.mutable.ArrayBuffer
+
 /*
  * MIT License
  *
@@ -22,8 +32,60 @@
  * SOFTWARE.
  */
 
-package org.multivacplatform.ml.util
+class CoNLLToDataframe extends Serializable {
 
-class CoNLLToDataframe {
+  def convertToDataFrame(inputCoNNLFilePath: String): DataFrame = {
+    val spark = ResourceHelper.spark
+    import spark.implicits._
+
+    val conf = new Job().getConfiguration
+    conf.set("textinputformat.record.delimiter", "\n\n")
+
+    val usgRDD = spark.sparkContext.newAPIHadoopFile(
+      inputCoNNLFilePath, classOf[TextInputFormat], classOf[LongWritable], classOf[Text], conf)
+      .map{ case (_, v) => v.toString }
+
+    val conllSentencesDF = usgRDD.map(s => s.split("\n")
+      .filter(x => !x.startsWith("# newdoc id"))
+    )
+      .filter(x => x.length > 0)
+      .toDF("sentence")
+
+    conf.set("textinputformat.record.delimiter", "")
+
+    conllSentencesDF
+      .withColumn("sent_id", extractCoNLLSentID($"sentence"))
+      .withColumn("text", extractCoNLLText($"sentence"))
+      .withColumn("ID", extractCoNLL($"sentence", lit(0)))
+      .withColumn("FORM", extractCoNLL($"sentence", lit(1)))
+      .withColumn("LEMMA", extractCoNLL($"sentence", lit(2)))
+      .withColumn("UPOS", extractCoNLL($"sentence", lit(3)))
+      .withColumn("XPOS", extractCoNLL($"sentence", lit(4)))
+      .withColumn("FEATS", extractCoNLL($"sentence", lit(5)))
+      .withColumn("HEAD", extractCoNLL($"sentence", lit(6)))
+      .withColumn("DEPREL", extractCoNLL($"sentence", lit(7)))
+      .withColumn("DEPS", extractCoNLL($"sentence", lit(8)))
+      .withColumn("MISC", extractCoNLL($"sentence", lit(9)))
+      .drop("sentence")
+
+  }
+
+  private def extractCoNLL = udf {(docs: Seq[String], index: Int) =>
+    var posTagsArray = ArrayBuffer[String]()
+    val newFilteredSentences =  docs.filter(x => !x.startsWith("# "))
+    for(e <- newFilteredSentences){
+      val splitedArray = e.split("\t")
+      posTagsArray += splitedArray(index)
+    }
+    posTagsArray
+  }
+
+  private def extractCoNLLSentID = udf {docs: Seq[String] =>
+    docs.head.split("# sent_id =").last
+  }
+
+  private def extractCoNLLText = udf {docs: Seq[String] =>
+    docs(1).split("# text =").last
+  }
 
 }
