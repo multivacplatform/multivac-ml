a001d2161e0556dd76910293264d1d0fa7a56bc5
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-01-13 17:24:10 +0100

    Download UD_Persian as well

--- download_conllu.sh
+++ download_conllu.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+echo "Downloading UD_English-EWT"
 wget -N https://github.com/UniversalDependencies/UD_English-EWT/raw/master/en_ewt-ud-train.conllu -P data/ud-treebanks-v2.3/
 wget -N https://github.com/UniversalDependencies/UD_English-EWT/raw/master/en_ewt-ud-dev.conllu -P data/ud-treebanks-v2.3/
 wget -N https://github.com/UniversalDependencies/UD_English-EWT/raw/master/en_ewt-ud-test.conllu -P data/ud-treebanks-v2.3/
 
+echo "Downloading UD_French-GSD"
 wget -N https://github.com/UniversalDependencies/UD_French-GSD/raw/master/fr_gsd-ud-train.conllu -P data/ud-treebanks-v2.3/
 wget -N https://github.com/UniversalDependencies/UD_French-GSD/raw/master/fr_gsd-ud-dev.conllu -P data/ud-treebanks-v2.3/
 wget -N https://github.com/UniversalDependencies/UD_French-GSD/raw/master/fr_gsd-ud-test.conllu -P data/ud-treebanks-v2.3/
+
+echo "Downloading UD_Persian-Seraji"
+wget -N https://github.com/UniversalDependencies/UD_Persian-Seraji/raw/master/fa_seraji-ud-train.conllu -P data/ud-treebanks-v2.3/
+wget -N https://github.com/UniversalDependencies/UD_Persian-Seraji/raw/master/fa_seraji-ud-dev.conllu -P data/ud-treebanks-v2.3/
+wget -N https://github.com/UniversalDependencies/UD_Persian-Seraji/raw/master/fa_seraji-ud-test.conllu -P data/ud-treebanks-v2.3/
